92216255cdd8d6ce28dae65c143ff75a201d88d9
Duc Minh Le <lemduc@gmail.com>
2021-08-01 16:32:16 +0700

    restfstool: added BackEndSoftware, updated ReadMe

--- modules/restfstool/src/example/java/org/jda/example/restfstool/rfsgen/CourseManAppGen.java
+++ modules/restfstool/src/example/java/org/jda/example/restfstool/rfsgen/CourseManAppGen.java
@@ -1,64 +1,15 @@
 package org.jda.example.restfstool.rfsgen;
 
-import java.util.List;
-import java.util.function.Consumer;
-
-import com.hanu.courseman.SCC1;
-import com.hanu.courseman.modules.ModuleMain;
-import com.hanu.courseman.modules.address.ModuleAddress;
-import com.hanu.courseman.modules.address.model.Address;
-import com.hanu.courseman.modules.coursemodule.ModuleCourseModule;
-import com.hanu.courseman.modules.coursemodule.model.CompulsoryModule;
-import com.hanu.courseman.modules.coursemodule.model.CourseModule;
-import com.hanu.courseman.modules.coursemodule.model.ElectiveModule;
-import com.hanu.courseman.modules.enrolment.ModuleEnrolment;
-import com.hanu.courseman.modules.enrolment.model.Enrolment;
-import com.hanu.courseman.modules.student.ModuleStudent;
-import com.hanu.courseman.modules.student.model.Gender;
-import com.hanu.courseman.modules.student.model.Student;
-import com.hanu.courseman.modules.studentclass.ModuleStudentClass;
-import com.hanu.courseman.modules.studentclass.model.StudentClass;
-
 import jda.modules.restfstool.RFSGen;
+import static org.jda.example.restfstool.rfsgen.Resources.*;
 
 /**
  * The software generator for CourseManApp.
  * @author binh_dh
  */
 public class CourseManAppGen {
-    // initialize the model
-    static final Class<?>[] model = {
-            CourseModule.class,
-            Enrolment.class,
-            Student.class,
-            Address.class,
-            StudentClass.class,
-            CompulsoryModule.class,
-            ElectiveModule.class
-    };
-    
-    static final Class<?>[] auxModel = {
-        Gender.class
-    };
-    
-    // initialize module classes
-    // one module per INHERITANCE TREE
-    static final Class<?>[] modules = {
-            ModuleCourseModule.class,
-            ModuleEnrolment.class,
-            ModuleStudent.class,
-            ModuleAddress.class,
-            ModuleStudentClass.class
-    };
-
     
     public static void main(String[] args) {
-      final String frontendOutputPath = "src/example/java/com/hanu/courseman/frontend";
-      final String backendTargetPackage = "com.hanu.courseman.backend";
-      final String backendOutputPath = "src/example/java";
-      final Class<?> scc = SCC1.class;
-      final Class<?> mccMain = ModuleMain.class;
-      final Consumer<List<Class>> runCallBack = new BackendMain(model);
       
       new RFSGen().run(
           // front-end config + some shared configs
---
+++ modules/restfstool/src/example/java/org/jda/example/restfstool/rfsgen/Resources.java
@@ -0,0 +1,63 @@
+package org.jda.example.restfstool.rfsgen;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.hanu.courseman.SCC1;
+import com.hanu.courseman.modules.ModuleMain;
+import com.hanu.courseman.modules.address.ModuleAddress;
+import com.hanu.courseman.modules.address.model.Address;
+import com.hanu.courseman.modules.coursemodule.ModuleCourseModule;
+import com.hanu.courseman.modules.coursemodule.model.CompulsoryModule;
+import com.hanu.courseman.modules.coursemodule.model.CourseModule;
+import com.hanu.courseman.modules.coursemodule.model.ElectiveModule;
+import com.hanu.courseman.modules.enrolment.ModuleEnrolment;
+import com.hanu.courseman.modules.enrolment.model.Enrolment;
+import com.hanu.courseman.modules.student.ModuleStudent;
+import com.hanu.courseman.modules.student.model.Gender;
+import com.hanu.courseman.modules.student.model.Student;
+import com.hanu.courseman.modules.studentclass.ModuleStudentClass;
+import com.hanu.courseman.modules.studentclass.model.StudentClass;
+
+/**
+ * @overview 
+ *
+ * @author Duc Minh Le (ducmle)
+ *
+ * @version 
+ */
+public class Resources {
+//initialize the model
+  public static final Class<?>[] model = {
+          CourseModule.class,
+          Enrolment.class,
+          Student.class,
+          Address.class,
+          StudentClass.class,
+          CompulsoryModule.class,
+          ElectiveModule.class
+  };
+  
+  public static final Class<?>[] auxModel = {
+      Gender.class
+  };
+  
+  // initialize module classes
+  // one module per INHERITANCE TREE
+  public static final Class<?>[] modules = {
+          ModuleCourseModule.class,
+          ModuleEnrolment.class,
+          ModuleStudent.class,
+          ModuleAddress.class,
+          ModuleStudentClass.class
+  };
+
+
+  public static final String frontendOutputPath = "src/example/java/com/hanu/courseman/frontend";
+  public static final String backendTargetPackage = "com.hanu.courseman.backend";
+  public static final String backendOutputPath = "src/example/java";
+  public static final Class<?> scc = SCC1.class;
+  public static final Class<?> mccMain = ModuleMain.class;
+  public static final Consumer<List<Class>> runCallBack = new BackendMain(model);
+
+}
