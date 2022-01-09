32e79d891868de969b176111006de543e7de979f
Duc Minh Le <lemduc@gmail.com>
2022-01-09 15:48:31 +0700

    moved to examples/

--- examples/jda-eg-coursemanrestful/src/main/resources/react/templates/react_view_form_body.js
+++
@@ -1,3 +0,0 @@
-<Form>
-  {{ form.body }}
-</Form>
--- examples/jda-eg-coursemanrestful/src/main/resources/react/templates/subform.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import BaseSubmodule from "../base/BaseSubmodule";
-import {{ view.name.module }} from "../{{ view.dir }}";
-
-export default class {{ view.name.submodule }} extends BaseSubmodule {
-  renderModule(props) {
-    return (<{{ view.name.module }} {...props} excludes={["{{ view.parent }}"]} />);
-  }
-}
