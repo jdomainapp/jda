45fad1b9430bf88cb4f9f4b5e43fb7284958a3ac
courtney1403 <38276404+courtney1403@users.noreply.github.com>
2022-04-01 19:21:20 +0700

    Add files via upload

---
+++ examples/courseman/feangular/src/app/base/base-form/base-form.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { BaseService } from 'src/app/base/base.service';
+import { NotificationService } from 'src/app/common/notification.service';
+import { BaseCommonComponent } from '../base-common/base-common.component';
+
+@Component({
+  selector: 'app-base-form',
+  template: '',
+  styleUrls: ['./base-form.component.css']
+})
+export class BaseFormComponent extends BaseCommonComponent implements OnInit {
+  show_child: boolean = false;
+  @Input() show_component: boolean = true;
+
+  // constructor(protected serv: BaseService, tst: NotificationService) {
+  //   super(serv, tst);
+  // }
+
+  override ngOnInit(): void {
+    this.service.init(this.apiName);
+    // this.loadComponent();
+  }
+
+  reset() {
+    this.item = {};
+  }
+
+  save() {
+      //dữ liệu từ child gửi vào
+      if (typeof this.item.id != 'undefined' && this.item.id) {
+        this.updateItem();
+      } else {
+        console.log('create:', this.item);
+        this.createItem();
+      }
+    }
+
+  search(search_id: any) {   
+      this.getItem(search_id);
+  }
+  
+  changeShowChild() {
+    this.show_child = !this.show_child;
+  }
+}
