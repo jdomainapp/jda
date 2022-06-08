b2e2d59948a72ce1dcfa9d21f19738a56b5097c8
haworker25 <haworker25@gmail.com>
2022-06-08 04:02:50 -0700

    Ha update

--- examples/courseman/msa/modules/gatewayserver/src/main/java/org/jda/example/coursemanmsa/gatewayserver/ApiGatewayServerApplication.java
+++ examples/courseman/msa/modules/gatewayserver/src/main/java/org/jda/example/coursemanmsa/gatewayserver/ApiGatewayServerApplication.java
@@ -3,6 +3,8 @@ package org.jda.example.coursemanmsa.gatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
