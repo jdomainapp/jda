11a505928f977c77528608563c1fda7c13a07aa6
linh.tq <tranquanglinh.pt@gmail.com>
2022-07-07 15:28:31 +0700

    update childModules in SCC courseman example

--- examples/courseman/mosar/src/main/java/org/jda/example/coursemanrestful/modules/studentclass/ModuleStudentClass.java
+++ examples/courseman/mosar/src/main/java/org/jda/example/coursemanrestful/modules/studentclass/ModuleStudentClass.java
@@ -13,56 +13,51 @@ import jda.modules.setup.commands.CopyResourceFilesCommand;
 import jda.mosa.controller.Controller;
 import jda.mosa.view.View;
 import jda.mosa.view.assets.panels.DefaultPanel;
-
-import java.util.List;
-
-import org.jda.example.coursemanrestful.modules.student.ModuleStudent;
 import org.jda.example.coursemanrestful.modules.student.model.Student;
 import org.jda.example.coursemanrestful.modules.studentclass.model.StudentClass;
 
+import java.util.List;
+
 /**
- * @overview 
- *  Represents the module descriptor of the software module that manages the domain objects of 
- *  <tt>SClass</tt>
- * 
  * @author dmle
+ * @overview Represents the module descriptor of the software module that manages the domain objects of
+ * <tt>SClass</tt>
  */
-@ModuleDescriptor(name="ModuleSClass",
-modelDesc=@ModelDesc(
-    model= StudentClass.class
-),
-viewDesc=@ViewDesc(
-    formTitle="Manage Student Classes",
-    domainClassLabel="Student Class",
-    imageIcon="sclass.jpg",
-    view=View.class,
-    viewType=RegionType.Data,
-    parent=RegionName.Tools
-),
-  controllerDesc=@ControllerDesc(
-      controller=Controller.class,
-      openPolicy=OpenPolicy.O_C
-      ),  // v2.6.4b
-  isPrimary=true,
-  childModules={ModuleStudent.class}
-  ,setUpDesc=@SetUpDesc(postSetUp=CopyResourceFilesCommand.class)
+@ModuleDescriptor(name = "ModuleSClass",
+        modelDesc = @ModelDesc(
+                model = StudentClass.class
+        ),
+        viewDesc = @ViewDesc(
+                formTitle = "Manage Student Classes",
+                domainClassLabel = "Student Class",
+                imageIcon = "sclass.jpg",
+                view = View.class,
+                viewType = RegionType.Data,
+                parent = RegionName.Tools
+        ),
+        controllerDesc = @ControllerDesc(
+                controller = Controller.class,
+                openPolicy = OpenPolicy.O_C
+        ),  // v2.6.4b
+        isPrimary = true,
+        setUpDesc = @SetUpDesc(postSetUp = CopyResourceFilesCommand.class)
 )
 public class ModuleStudentClass {
-  @AttributeDesc(label="Student Class")
-  private String title;
+    @AttributeDesc(label = "Student Class")
+    private String title;
 
-  @AttributeDesc(label="Id")
-  private int id;
+    @AttributeDesc(label = "Id")
+    private int id;
 
-  @AttributeDesc(label="Name")
-  private String name;
-  
-  @AttributeDesc(label="Students",type= DefaultPanel.class,
-      controllerDesc=@ControllerDesc(
-          //openPolicy=OpenPolicy.O
-          /*testing: load all student objects with children*/
-          openPolicy=OpenPolicy.L_C
-        )
-      )
-  private List<Student> students;
+    @AttributeDesc(label = "Name")
+    private String name;
+
+    @AttributeDesc(label = "Students", type = DefaultPanel.class,
+            controllerDesc = @ControllerDesc(
+                    //openPolicy=OpenPolicy.O
+                    /*testing: load all student objects with children*/
+                    openPolicy = OpenPolicy.L_C
+            )
+    )
+    private List<Student> students;
 }
