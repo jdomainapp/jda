0eaacf627db740f89215ec8251ea0c7a357a1c4c
Ha Vu <89120031+haworker25@users.noreply.github.com>
2022-09-22 11:11:31 +0700

    Add msatool

---
+++ examples/courseman/msa/modules/msatool/src/main/java/org/jda/eg/coursemanmsa/msatool/ServiceReconfigurer.java
@@ -0,0 +1,108 @@
+package org.jda.eg.coursemanmsa.msatool;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServiceReconfigurer {
+
+	private static final Logger logger = LoggerFactory.getLogger(ServiceReconfigurer.class);
+
+	/**
+	 * Sending jar file over network
+	 */
+	public void sendJar(String serverHost, int serverPort, String filePath) {
+		Socket socket = null;
+		InputStream in = null;
+		OutputStream out = null;
+		try {
+			socket = new Socket(serverHost, serverPort);
+			if (filePath.endsWith(".jar")) {
+				File jarFile = new File(filePath);
+				long length = jarFile.length();
+				in = new FileInputStream(jarFile);
+				out = socket.getOutputStream();
+
+				byte[] buffer = new byte[4069];
+				int count;
+				while ((count = in.read(buffer)) > 0) {
+					out.write(buffer, 0, count);
+				}
+				out.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			logger.error(e.getMessage() + ": Error when sending jar file");
+		} finally {
+			try {
+				if (out != null)
+					out.close();
+				if (in != null)
+					in.close();
+				if (socket != null)
+					socket.close();
+			} catch (Exception e) {
+				logger.error(e.getMessage()+": Error closing");
+			}
+		}
+	}
+
+	/**
+	 * Receive jar file
+	 */
+	public void receiveJar(int listenPort, String filePath) {
+
+		InputStream in = null;
+		OutputStream out = null;
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+
+		try {
+			serverSocket = new ServerSocket(listenPort);
+			// always listen connections from sending side
+			while (true) {
+				try {
+					logger.info("Waiting...");
+					socket = serverSocket.accept();
+					logger.info("Accepted connection : " + socket);
+					in = socket.getInputStream();
+					out = new FileOutputStream(new File(filePath));
+					int count;
+					byte[] buffer = new byte[4096];
+					while ((count = in.read(buffer)) > 0) {
+						out.write(buffer, 0, count);
+					}
+					out.flush();
+				} catch (IOException ex) {
+					ex.printStackTrace();
+					logger.error(ex.getMessage() + ": An Inbound Connection Was Not Resolved");
+				} finally {
+					if (out != null)
+						out.close();
+					if (in != null)
+						in.close();
+					if (socket != null)
+						socket.close();
+				}
+			}
+		} catch (IOException e) {
+			logger.error(e.getMessage() + ": Error when receiving Jar file!!!");
+		} finally {
+			if (serverSocket != null)
+				try {
+					serverSocket.close();
+				} catch (IOException e) {
+					logger.error(e.getMessage());
+				}
+		}
+	}
+}
---
+++ examples/courseman/msa/modules/msatool/src/test/java/org/jda/eg/coursemanmsa/msatool/ReceiverTest.java
@@ -0,0 +1,12 @@
+package org.jda.eg.coursemanmsa.msatool;
+
+import java.io.File;
+
+public class ReceiverTest {
+    public static void main(String[] args) {
+    	ServiceReconfigurer receiver = new ServiceReconfigurer();
+    	String receiverLocation = System.getProperty("user.dir")+File.separator+"execute";
+    	String fileName = "coursemodule-service.jar";
+    	receiver.receiveJar(4444, receiverLocation+File.separator+fileName);
+    }
+}
