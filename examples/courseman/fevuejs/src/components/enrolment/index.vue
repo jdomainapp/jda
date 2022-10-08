b9198a474760f723962e34ce729949a1b0d57883
kidu20 <73027951+kidu20@users.noreply.github.com>
2022-10-08 04:12:58 +0200

    Update index.vue

--- examples/courseman/fevuejs/src/components/enrolment/index.vue
+++ examples/courseman/fevuejs/src/components/enrolment/index.vue
@@ -1,56 +1,48 @@
-<table class="table table-bordered table-hover">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Id</th>
-            <th>Student Name</th>
-            <th>Coursemodule</th>
-            <th>Internalmark</th>
-            <th>Exammark</th>
-            <th>Finalgrade</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
+<template src="./template/index.html"></template>
+<script>
 
-        <tr style="cursor: pointer;" :key="enrolment.id" v-for="(enrolment, index) in enrolments.content" v-if="parentID === ''">
-            <td style="vertical-align: middle;">{{index + 1}}</td>
-            <td style="vertical-align: middle;">{{enrolment.id}}</td>
-            <td style="vertical-align: middle;">{{enrolment.student.name}}</td>
-            <td style="vertical-align: middle;">{{enrolment.courseModule.name}}</td>
-            <td style="vertical-align: middle;">{{enrolment.internalMark}}</td>
-            <td style="vertical-align: middle;">{{enrolment.examMark}}</td>
-            <td style="vertical-align: middle;">{{enrolment.finalGrade}}</td>
-            <td style="vertical-align: middle; width: 200px;">
-                <button class="btn btn-primary" v-on:click="emitData(enrolment)">
-                    Edit
-                </button>
-                <a class="btn btn-danger" id="btnShowModal" data-target="#myModal" v-on:click="getEnrolmentId(enrolment.id)" data-toggle="modal">
-                    Delete
-                </a>
-            </td>
-        </tr>
+    export default {
+        props: {
+            parentData:Object
+        },
 
+        components: {
+            "form-add": () => import('./add.vue'),
+            "form-list": () => import('./list.vue'),
+        },
 
-        <!-- <tr style="cursor: pointer;" :key="enrolment.id" v-for="(enrolment, index) in enrolments.content" v-if="(enrolment.student.name === enrolmentIn) && (eDisplay === 2)"> -->
-        <tr style="cursor: pointer;" :key="enrolment.id" v-for="(enrolment, index) in enrolments.content" v-if="(enrolment.student.id === parentID) && (parentID != undefined)">
-            <td style="vertical-align: middle;">{{index + 1}}</td>
-            <td style="vertical-align: middle;">{{enrolment.id}}</td>
-            <td style="vertical-align: middle;">{{enrolment.student.name}}</td>
-            <td style="vertical-align: middle;">{{enrolment.courseModule.name}}</td>
-            <td style="vertical-align: middle;">{{enrolment.internalMark}}</td>
-            <td style="vertical-align: middle;">{{enrolment.examMark}}</td>
-            <td style="vertical-align: middle;">{{enrolment.finalGrade}}</td>
-            <td style="vertical-align: middle; width: 200px;">
-                <button class="btn btn-primary" v-on:click="emitData(enrolment)">
-                    Edit
-                </button>
-                <a class="btn btn-danger" id="btnShowModal" data-target="#myModal" v-on:click="getEnrolmentId(enrolment.id)" data-toggle="modal">
-                    Delete
-                </a>
-            </td>
-        </tr>
-    </tbody>
-    <Toasts></Toasts>
-    <modal-confirm @confirm="deleteEnrolment(enrolmentId)"></modal-confirm>
-</table>
+        data() {
+            return {
+                display: 1,
+                enrolmentId: 0,
+                address: '',
+                dataSubForm: {
+                    mode: "create",
+                    enrolment: null
+                },
+            }
+        },
+
+        mounted() {   
+            
+            if(this.data != undefined){
+                if (this.data.parent !== null) {
+                    this.dataSubForm = this.data;
+                }
+            }
+        },
+
+        methods: {
+            setData(data){
+                this.dataSubForm.enrolment = data.enrolment;
+                this.display = 1;
+                this.dataSubForm.mode = data.mode;
+            },
+
+            mainForm() {
+                this.display = 2;
+                this.dataSubForm.mode = "create";
+            }
+        }
+    };
+</script>
