647d3a7bbcdf04af2ce4bacdfdb44c4bbb0f515b
havt <haworker25@gmail.com>
2023-06-21 13:19:46 -0700

    Refactor

--- modules/msacommon/src/main/java/jda/modules/msacommon/controller/GetByAssociation.java
+++
@@ -1,7 +0,0 @@
-package jda.modules.msacommon.controller;
-
-import java.util.Collection;
-
-public abstract class GetByAssociation <V, T>{
-	abstract void execute(V propertyValue, String propertyName, Collection<T> records);
-}
