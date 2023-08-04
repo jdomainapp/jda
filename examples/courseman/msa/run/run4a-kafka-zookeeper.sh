2d08b77bbaea1e7e0ff34706fe2cfe23801dce7c
Duc Minh Le <lemduc@gmail.com>
2023-08-04 17:05:01 +0700

    add input parameter support

--- examples/courseman/msa/run/run4a-kafka-zookeeper.sh
+++ examples/courseman/msa/run/run4a-kafka-zookeeper.sh
@@ -1,7 +1,12 @@
 #
 # run KAFKA service with Courseman's topics
 #
-kafka_dir="/data/programs/kafka"
+if [ -z $1 ]
+then
+  kafka_dir="/data/programs/kafka"
+else
+  kafka_dir=$1
+fi
 
 # Start the ZooKeeper service
 $kafka_dir/bin/zookeeper-server-start.sh $kafka_dir/config/zookeeper.properties
