14aea737653b26943fadaf899f5731c2206315ff
nitromelon <github.z6b5z@simplelogin.com>
2023-12-02 15:35:58 +0700

    Rewrite files in router folder Add demo.vue as page to test design patterns. Add the first design pattern: date range picker. Other new file: .vscode/settings.json in root folder.

---
+++ examples/courseman/fevuejs/src/components/demo/README.md
@@ -0,0 +1,6 @@
+# Demo page
+
+- Date created: 02/12/2023
+- Last updated: 02/12/2023
+- Status: **In progress**
+- Team: Dung Tri, My Chi, Duc Anh
--- examples/courseman/fevuejs/src/router/name.js
+++ examples/courseman/fevuejs/src/router/name.js
@@ -1,17 +1,24 @@
-export const HOME_NAME = 'home';
+export const HOME_NAME = "home";
 
-export const STUDENT_CLASS_INDEX_NAME = 'student-class';
-export const STUDENT_CLASS_LIST_NAME = 'student-class-list';
-export const STUDENT_CLASS_ADD_NAME = 'student-class-add';
-export const ADDRESS_INDEX_NAME = 'address';
-export const ADDRESS_LIST_NAME = 'address-list';
-export const ADDRESS_ADD_NAME = 'address-add';
-export const COURSE_MODULE_INDEX_NAME = 'course-module';
-export const COURSE_MODULE_LIST_NAME = 'course-module-list';
-export const COURSE_MODULE_ADD_NAME = 'course-module-add';
-export const ENROLMENT_INDEX_NAME = 'enrolment';
-export const ENROLMENT_LIST_NAME = 'enrolment-list';
-export const ENROLMENT_ADD_NAME = 'enrolment-add';
-export const STUDENT_INDEX_NAME = 'student';
-export const STUDENT_LIST_NAME = 'student-list';
-export const STUDENT_ADD_NAME = 'student-add';
+export const STUDENT_CLASS_INDEX_NAME = "student-class";
+export const STUDENT_CLASS_LIST_NAME = "student-class-list";
+export const STUDENT_CLASS_ADD_NAME = "student-class-add";
+
+export const ADDRESS_INDEX_NAME = "address";
+export const ADDRESS_LIST_NAME = "address-list";
+export const ADDRESS_ADD_NAME = "address-add";
+
+export const COURSE_MODULE_INDEX_NAME = "course-module";
+export const COURSE_MODULE_LIST_NAME = "course-module-list";
+export const COURSE_MODULE_ADD_NAME = "course-module-add";
+
+export const ENROLMENT_INDEX_NAME = "enrolment";
+export const ENROLMENT_LIST_NAME = "enrolment-list";
+export const ENROLMENT_ADD_NAME = "enrolment-add";
+
+export const STUDENT_INDEX_NAME = "student";
+export const STUDENT_LIST_NAME = "student-list";
+export const STUDENT_ADD_NAME = "student-add";
+
+// demo design pattern:
+export const DEMO_NAME = "demo";
