5349a824b21aab07655e9f5037837e70fe2ffbe2
nitromelon <github.z6b5z@simplelogin.com>
2023-12-10 21:29:58 +0700

    Complete input design patterns. Changes to be committed: 	modified:   examples/courseman/fevuejs/package-lock.json 	modified:   examples/courseman/fevuejs/package.json 	modified:   examples/courseman/fevuejs/src/components/demo/demo.vue 	modified:   examples/courseman/fevuejs/src/components/demo/template/demo.html 	modified:   examples/courseman/fevuejs/yarn.lock 	modified:   examples/courseman/pom.xml

--- examples/courseman/fevuejs/src/components/demo/demo.vue
+++ examples/courseman/fevuejs/src/components/demo/demo.vue
@@ -1,16 +1,26 @@
 <template src="./template/demo.html"></template>
 
 <script>
+// date range picker
 import DateRangePicker from "vue2-daterange-picker";
 import "vue2-daterange-picker/dist/vue2-daterange-picker.css";
+
+// vue slider
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
+
+// rating
+import StarRating from "vue-star-rating";
 
 let startDate = new Date();
 let endDate = new Date();
 
 export default {
-    components: { DateRangePicker },
+    components: { DateRangePicker, VueSlider, StarRating },
+
     data() {
         return {
+            // Date range picker
             dateRange: { startDate, endDate },
             options: {
                 format: "DD/MM/YYYY",
@@ -26,8 +36,12 @@ export default {
                     cancelLabel: "Clear",
                 },
             },
+
+            // Vue slider
+            range: [0, 30],
         };
     },
+
     filters: {
         date(val) {
             return val ? val.toLocaleString() : "";
@@ -39,5 +53,13 @@ export default {
 <style scoped>
 .input_date {
     min-width: 350px;
+}
+
+span.input {
+    display: block;
+    width: 100%;
+    border: 1px solid black;
+    resize: none;
+    padding: 4px;
 }
 </style>
