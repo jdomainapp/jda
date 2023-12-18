6ba8553165507c7e315d9e751016eea4a59569e8
nitromelon <github.z6b5z@simplelogin.com>
2023-12-18 13:13:16 +0700

    minor modify for vue's sidebar

--- examples/courseman/fevuejs/src/lib/NestedNav.vue
+++ examples/courseman/fevuejs/src/lib/NestedNav.vue
@@ -2,14 +2,37 @@
 <template>
     <nav class="nav nav-pills flex-column">
         <div v-for="(item, index) in filteredItems" :key="index">
+            <!-- click = prevent default for only one has nested nav -->
+            <!-- Todo: rewrite this code to more readable -->
             <a
                 class="nav-link"
+                :class="{
+                    'has-children': item.children && item.children.length,
+                    'is-open': showNestedNav[index],
+                }"
                 :href="'#' + item.name.toLowerCase().replace(/ /g, '_')"
+                @click="
+                    item.children && item.children.length
+                        ? toggleNestedNav($event, index)
+                        : null
+                "
             >
+                <span v-if="item.children && item.children.length">
+                    <img
+                        v-if="showNestedNav[index]"
+                        :src="arrow.down"
+                        alt="-"
+                    />
+                    <img v-else :src="arrow.right" alt="+" />
+                </span>
                 {{ item.name }}
             </a>
             <nested-nav
-                v-if="item.children && item.children.length"
+                v-if="
+                    item.children &&
+                    item.children.length &&
+                    showNestedNav[index]
+                "
                 :items="item.children"
                 class="nav nav-pills flex-column ml-4"
             ></nested-nav>
@@ -18,6 +41,9 @@
 </template>
 
 <script>
+import rightArrow from "../assets/img/NestedNav/arrow_right.svg";
+import downArrow from "../assets/img/NestedNav/arrow_down.svg";
+
 // The `filterItems` function is a recursive function that filters the items based on a search query.
 function filterItems(item, query) {
     if (item.name.toLowerCase().includes(query)) {
@@ -47,24 +73,34 @@ export default {
         searchQuery: {
             type: String,
             default: "",
-            required: true,
         },
     },
-
+    data() {
+        return {
+            // Hide all nested navs by default
+            showNestedNav: this.items.map(() => false),
+            arrow: {
+                right: rightArrow,
+                down: downArrow,
+            },
+        };
+    },
+    methods: {
+        toggleNestedNav(event, index) {
+            event.preventDefault();
+            this.$set(this.showNestedNav, index, !this.showNestedNav[index]);
+        },
+    },
     computed: {
         filteredItems() {
-            const query = this.searchQuery.trim().toLowerCase();
-            if (!query) {
-                return this.items;
-            }
             return this.items
-                .map((item) => filterItems(item, query))
+                .map((item) => filterItems(item, this.searchQuery))
                 .filter((x) => x);
         },
     },
 };
 </script>
 
-<style>
+<style scoped>
 /* Future style */
 </style>
