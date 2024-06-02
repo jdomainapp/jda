37f08c64bb436f4da02acd2cb6f2f19f4775721c
linh.tq <linh.tq@samsung.com>
2024-06-02 11:53:44 +0700

    gen app-routing.module.ts

--- modules/mosar/src/main/resources/fe/angular/src/app/app-routing.module.ts
+++ modules/mosar/src/main/resources/fe/angular/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';@loop{importModuleComponent}[[
-import { @slot{{ModuleName}}Component } from './@slot{{moduleJname}}/@slot{{moduleJname}}.component';]]loop{importModuleComponent}@
+import { @slot{{ModuleName}}Component } from './@slot{{moduleJname}}/components/@slot{{moduleJname}}/@slot{{moduleJname}}.component';]]loop{importModuleComponent}@
 
 const routes: Routes = [@loop{routes}[[
-    {path:'@slot{{moduleJnames}}' , component:@slot{{ModuleName}}Component, pathMatch: 'full'},]]loop{routes}@
+    { path: '@slot{{moduleJnames}}', component: @slot{{ModuleName}}Component },]]loop{routes}@
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+      RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
